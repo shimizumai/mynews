bc9f4d72cfc95b19f9da5e20390df699687820ff
mai <shimizu12mai24@gmail.com>
2021-06-10 12:33:02 +0000

    kadai

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class NewsController extends Controller
+{
+    //
+    public function add()
+    {
+        return view('adimin.news.create');
+    }
+}
+//【応用】 artisanを使って、Admin/ProfileControllerを作成しましょう
