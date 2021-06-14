f3bab522ff0a96d23eda497a1b8444ea6080c505
mai <shimizu12mai24@gmail.com>
2021-06-14 12:06:12 +0000

    kadai6/14

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+/*「http://XXXXXX.jp/XXX というアクセスが来たときに、
+AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください*/
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('XXX',
+'AAAController@bbb');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('admin/profile/create',
+    'ProfileController@add');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('admin/profile/edit',
+    'ProfileController@adit');
+});
